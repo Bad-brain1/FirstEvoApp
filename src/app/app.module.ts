90bceeda70495c74bf29f291207c1519129c06fd
Alexandr Radynov <radynov2014@yandex.ru>
2022-11-28 15:44:40 +0300

    lesson 1.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { SumPipe } from './sum.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    SumPipe
   ],
   imports: [
     BrowserModule,
